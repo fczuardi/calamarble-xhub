9f552bf8735c135010fc270ab6f67ff3addd3a00
Fabricio C Zuardi <fabricio@fabricio.org>
2016-07-27 18:52:15 -0300

    more log

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ const signSha1 = (secret, msg) =>
     crypto.createHmac('sha1', secret).update(msg).digest('hex');
 
 const apiEndpoint = config => (req, res) => {
-    const serverSignature = signSha1(config.xHubSecret, JSON.stringify(req.body));
+    const rawBody = req.rawBody || JSON.stringify(req.body);
+    const xHubSignature = req.headers['X-Hub-Signature'];
+    const serverSignature = signSha1(config.xHubSecret, rawBody);
     const result = {
         ...req.headers,
         'my-debug-header': serverSignature
     };
     console.log('serverSignature:', serverSignature);
+    console.log('X-Hub-Signature', xHubSignature);
     console.log('HEADERS', req.headers);
     console.log('RAW BODY', req.rawBody);
+    console.log('RAW BODY TYPE', typeof req.rawBody);
     console.log('BODY', req.body);
     console.log('stringified BODY', JSON.stringify(req.body));
     return res ? res.send(result) : result;
