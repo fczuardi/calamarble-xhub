7ca05fc8fbf976ccc726f83b966c91ebc878e277
Fabricio C Zuardi <fabricio@fabricio.org>
2016-07-28 00:25:26 -0300

    return error on signature mismatch, get algorithm and error message from config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import crypto from 'crypto';
 
 
-const signSha1 = (secret, msg) =>
-    crypto.createHmac('sha1', secret).update(msg).digest('hex');
+const signature = (algo, secret, msg) =>
+    crypto.createHmac(algo, secret).update(msg).digest('hex');
 
 const apiEndpoint = config => (req, res) => {
     const rawBody = req.rawBody || JSON.stringify(req.body);
     const xHubSignature = req.headers['X-Hub-Signature'];
-    let serverSignature;
-    try {
-        serverSignature = signSha1(config.xHubSecret, rawBody);
-    } catch (e) {
-        serverSignature = e.message;
-    }
+    const serverSignature = signature(config.xHubAlgo, config.xHubSecret, rawBody);
     const result = {
         ...req.headers,
         'my-debug-header': serverSignature
     };
+    const signatureMatches = xHubSignature.split('=')[1] === serverSignature;
     console.log('serverSignature:', serverSignature);
     console.log('X-Hub-Signature', xHubSignature);
     console.log('rawBody', rawBody);
+    consolr.log('signature matches', signatureMatches);
+    if (!signatureMatches) {
+        console.error(config.messages.wrongSignature);
+        throw config.messages.wrongSignature;
+    }
     return res ? res.send(result) : result;
 };
 
